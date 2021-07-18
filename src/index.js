e760c1275ea28447a64d74da104122fca842dd83
MariaOliinyk <m.v.oliynyk@gmail.com>
2021-07-18 18:51:53 +0300

    fixed hero-header
    fixed hero-header
    fixed common

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import './sass/main.scss';
 // Import a module using ES6 import syntax
 import './js/mobile-menu.js'
-
-// Import a module from a URL (e.g. CDN), using dynamic imports
-import('https://cdnjs.cloudflare.com/ajax/libs/body-scroll-lock/3.1.5/bodyScrollLock.min.js').then(() => {
-  console.log(_.VERSION);
-});
