66628c3bd56baa3d681360c7a998d07d30c67b4d
RiaboshlykVeronika <82654040+RiaboshlykVeronika@users.noreply.github.com>
2021-07-19 15:51:37 +0000

    Deploying to gh-pages from @ RiaboshlykVeronika/project-group14@f2a25aad7dd4238f03dfc158efe37865e93068a0 🚀

--- src.588d1cf2.js
+++ src.588d1cf2.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu.js");
 },{"./sass/main.scss":"clu1","./js/mobile-menu.js":"sByI"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group14/src.668e0165.js.map
+//# sourceMappingURL=/project-group14/src.588d1cf2.js.map
